524b8f6225d8aee66b1b2a6eafb23f85db48f3ec
Eliezer Oliveira <eliezerreis@snews.tv>
2025-02-02 21:07:58 -0500

    Add the service discover Eureka to the project.

--- configserver/src/main/java/com/ezbank/configserver/configServer/ConfigServerApplication.java
+++ configserver/src/main/java/com/ezbank/configserver/configServer/ConfigServerApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-public class ParameterStoreApplication {
+public class ConfigServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ParameterStoreApplication.class, args);
+		SpringApplication.run(ConfigServerApplication.class, args);
 	}
 
 }
